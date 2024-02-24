fd7db92d6bb5b536894386252dfd1f83e059ec51
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-02-23 19:52:38 -0500

    Update Circenses.py

--- Circenses.py
+++ Circenses.py
@@ -1,15 +1,13 @@
 for i in range(int(input())):
   M, N = tuple(map(int, input().split()))
   Money=tuple(map(int, input().split()))
-  MoneyBack=[None]*M
+  MoneyBack=[0]*M
   i=0
-  h=0
-  while Money and h≤N:#verificando que haya dinero para repartir
-    MoneyBack[i]+=Money[h]
-    Money.remove(Money[h]) #eliminando los billetes ya tomados
+  for h in range(N):
+    MoneyBack[i]+=Money[0]
+    Money=Money[1:]
     if i==(M-1):
         i=0
     else:
         i+=1
-    h+=1
   print(max(MoneyBack)-min(MoneyBack))
