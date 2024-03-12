2704ded7e4ceb52d7d82ec8a67726fc70d29dc8a
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-03-12 09:51:13 -0500

    Update ListMean.py

--- ListMean.py
+++ ListMean.py
@@ -4,14 +4,13 @@ while True:
     Num=int(input())
     if Num==0:break
     LstM.append(Num)
-    LstM.append(Num)
     if len(LstM)%5==0:
         Yayo=sorted(LstM)
         if len(Yayo)%2==0:
-            Bam=(Yayo[len(Yayo)/2 -1]+Yayo[len(Yayo)])
+            Bam=(Yayo[int(len(Yayo)/2)-1]+Yayo[int(len(Yayo)/2)])
             if Bam%2!=0:
                 print(f'{Bam}/2')
             else:print(int(Bam/2))
         else:
-            Kaos=Yayo[int(len(Yayo)+0.5)]
+            Kaos=Yayo[int(len(Yayo)/2-0.5)]
             print(Kaos)
