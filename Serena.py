9ee31a8f96cf22885777b4e52e705763fc0932d9
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-03-13 14:24:46 -0500

    Create Serena.py

---
+++ Serena.py
@@ -0,0 +1,16 @@
+Fila=[]
+while True:
+    Persona=input().split()
+    if Persona==['0','0']: print(len(LstP)); break
+    Persona[1]=int(Persona[1])
+    if len(Fila)<Persona[1]: Fila.append(Persona)
+    for P in Fila:
+        if len(Fila)>=P[1]:Fila.pop(P)
+
+A 1
+B 2
+C 2
+D 3
+0 0
+
+2
