9991f221dbd8661822f8807a0f5fcf218f76ca51
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-03-14 17:55:47 -0500

    Update NumericKeyboard.py

--- NumericKeyboard.py
+++ NumericKeyboard.py
@@ -7,11 +7,5 @@ while True:
     elif len(Lst)!=0 and Type[0]=='C': Lst.pop()
     elif Type[0]=='D' and len(Lst)-int(Type[1])>=0:
         for _ in range(int(Type[1])): Lst.pop()                                                                                                                                                                                                                                          )
-    elif len(Type)==3 and int(Type[1])<=len(Lst) and int(Type[2])<=len(Lst):
-        for i in range(int(Type[1]),int(Type[2])+1):
-                    sum+=Lst[i-1]
-                    if i==int(Type[1]) or i!=int(Type[2]):#solo multiplicamos por 10, para sumarle, cuando es el primer elemento a sumar
-                        sum*=10
-                    else:
-                        break
-        print(sum)
+    elif len(Type)==3 and 0<=int(Type[1])<=len(Lst) and 0<=int(Type[2])<=len(Lst):
+        print(''.join(map(str,Lst[int(Type[1])-1:int(Type[2])])))
