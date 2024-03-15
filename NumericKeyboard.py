47012298354e3e398de237491f01e926cea62c4c
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-03-14 19:27:15 -0500

    Update NumericKeyboard.py

--- NumericKeyboard.py
+++ NumericKeyboard.py
@@ -1,11 +1,9 @@
 Lst=[]
 while True:
-    sum=0
     Type=input().split()
     if Type[0]=='end': break
-    elif len(Type)==1 and (Type[0]!='C' or Type[0]!='end'): Lst.append(int(Type[0])
+    elif len(Type)==1 and Type[0] not in ['end','C']: Lst.append(int(Type[0])
     elif len(Lst)!=0 and Type[0]=='C': Lst.pop()
-    elif Type[0]=='D' and len(Lst)-int(Type[1])>=0:
-        for _ in range(int(Type[1])): Lst.pop()                                                                                                                                                                                                                                          )
-    elif len(Type)==3 and 0<=int(Type[1])<=len(Lst) and 0<=int(Type[2])<=len(Lst):
+    elif Type[0]=='D' and len(Lst)-int(Type[1])>=0: Lst=Lst[:-int(Type[1])]                                                                                                                                                                                                                                         )
+    elif Type[0]=='M' and (0<=int(Type[1])<=len(Lst)) and 0<=int(Type[2])<=len(Lst):
         print(''.join(map(str,Lst[int(Type[1])-1:int(Type[2])])))
