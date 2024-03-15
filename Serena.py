5d1b1bab2be40f307eb10e4fc3fbe68d2acba894
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-03-15 09:36:44 -0500

    Update Serena.py

--- Serena.py
+++ Serena.py
@@ -4,8 +4,8 @@ while True:
     if Persona==['0','0']: print(len(Fila)); break
     Persona[1]=int(Persona[1])
     if len(Fila)<Persona[1]: Fila.append(Persona)
-    for P in Fila:#siempre lo hara
-        if len(Fila)>=P[1]:Fila.pop(Fila.index(P))
+    for P in Fila:
+        if len(Fila)>P[1]:Fila.pop(Fila.index(P))
 
 A 1
 B 2
