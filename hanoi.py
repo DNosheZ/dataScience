99c7d1f34f25018b8293c40f1d492c7f130bc7bf
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-08-11 21:01:23 -0500

    v.2.2.2

--- hanoi.py
+++ hanoi.py
@@ -4,45 +4,41 @@ for _ in range(int(input())):
     A_bar=deque([i for i in range(1, x+1)])
     B_bar=deque()
     C_bar=deque()
+    h=0
     while True:
-        if C_bar==deque([k for k in range(1, x+1)]):
-            print('soluciona la torre')
-            break
         A,B=map(str,input().split(' '))
         if A=='X' and B=="X":
-            if C_bar!=deque([j for j in range(1,x+1)]):
+            if h>0:
+                print('secuencia invalida')
+                break
+            elif C_bar!=deque([j for j in range(1,x+1)]):
                 print('no soluciona la torre')
+            
             else:print('soluciona la torre')
             break
         elif A!=B:
             if A=='A' and B=='B':
                 if len(A_bar)==0 or (len(B_bar)>0 and A_bar[0]>=B_bar[0]):
-                    print('secuencia invalida')
-                    break
+                    h+=1
                 else:B_bar.appendleft(A_bar.popleft())
             elif A=='A' and B=='C':
                 if len(A_bar)==0 or (len(C_bar)>0 and A_bar[0]>=C_bar[0]):
-                    print('secuencia invalida')
-                    break
+                    h+=1
                 else:C_bar.appendleft(A_bar.popleft())
             elif A=='B' and B=='A':
                 if len(B_bar)==0 or (len(A_bar)>0 and B_bar[0]>=A_bar[0]):
-                    print('secuencia invalida')
-                    break
+                    h+=1
                 else:A_bar.appendleft(B_bar.popleft())
             elif A=='B' and B=='C':
                 if len(B_bar)==0 or (len(C_bar)>0 and B_bar[0]>=C_bar[0]):
-                    print('secuencia invalida')
-                    break
+                    h+=1
                 else:C_bar.appendleft(B_bar.popleft())
             elif A=='C' and B=='A':
                 if len(C_bar)==0 or (len(A_bar)>0 and C_bar[0]>=A_bar[0]):
-                    print('secuencia invalida')
-                    break
+                    h+=1
                 else:A_bar.appendleft(C_bar.popleft())
             elif A=='C' and B=='B':
                 if len(C_bar)==0 or (len(B_bar)>0 and C_bar[0]>=B_bar[0]):
-                    print('secuencia invalida')
-                    break
+                    h+=1
                 else:B_bar.appendleft(C_bar.popleft())
         else: continue
