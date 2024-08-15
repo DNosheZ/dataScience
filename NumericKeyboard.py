09715a905f2f89b5aa1506905da016fa06736311
Alejandro Diaz <129427891+DNosheZ@users.noreply.github.com>
2024-08-14 21:06:05 -0500

    v.2.3.1
    Redo, having Runtime error on test 5

--- NumericKeyboard.py
+++ NumericKeyboard.py
@@ -1,9 +1,10 @@
-Lst=[]
+digs=[]
 while True:
-    Type=input().split()
-    if Type[0]=='end': break
-    elif len(Type)==1 and Type[0] not in ['end','C']: Lst.append(int(Type[0])
-    elif len(Lst)!=0 and Type[0]=='C': Lst.pop()
-    elif Type[0]=='D' and len(Lst)-int(Type[1])>=0: Lst=Lst[:-int(Type[1])]                                                                                                                                                                                                                                         )
-    elif Type[0]=='M' and (0<=int(Type[1])<=len(Lst)) and 0<=int(Type[2])<=len(Lst):
-        print(''.join(map(str,Lst[int(Type[1])-1:int(Type[2])])))
+    Tecla=input().split(' ')
+    if Tecla[0]=='C': digs.pop()
+    elif Tecla[0]=='D' and len(digs)>=int(Tecla[1]):
+        for _ in range(int(Tecla[1])):digs.pop()
+    elif Tecla[0]=='M' and len(digs)>=int(Tecla[2]):
+        print(''.join(map(str,digs[int(Tecla[1])-1:int(Tecla[2])])))
+    elif Tecla[0]=='end':break
+    elif len(Tecla)==1:digs.append(int(Tecla[0]))
